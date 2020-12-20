74b2ba4b8a388e6acae7dbb4c14d111436d3be80
jcgrethe <jgrethe@itba.edu.ar>
2020-12-20 18:28:55 -0300

    gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py init_data
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn covid_api.wsgi --workers 3 --timeout 600 --bind 0.0.0.0:8000
